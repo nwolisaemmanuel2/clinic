e8af8ee0613c1f8fda121dea0ed8808eddeeaa76
Nwolisa Emmanuel Ekene <nwolisaemmanuel2@gmail.com>
2021-01-16 13:39:27 +0000

    updated file

---
+++ patients/add_patient.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include('../dbconnect.php');
+$a = $_POST['firstName'];
+$b = $_POST['lastName'];
+$c = $_POST['tel'];
+$d = $_POST['age'];
+$e = $_POST['sex'];
+$f = $_POST['amount'];
+$g = $_POST['address'];
+$h = $_POST['ins'];
+//$h = $_POST['date'];
+// query
+$sql = "INSERT INTO patient (firstName,lastName,tel,age,sex,amount,address,ins) VALUES (:a,:b,:c,:d,:e,:f,:g,:h)";
+$q = $db->prepare($sql);
+$q->execute(array(':a' => $a, ':b' => $b, ':c' => $c, ':d' => $d, ':e' => $e, ':f' => $f, ':g' => $g, ':h' => $h));
+header("location: index.php");
+
+
+?>
